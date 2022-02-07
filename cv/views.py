7c6fef3a4b08582c8820077343d27164ca296803
José Pedro Silva Aguiar <joseaguiar@student.dei.uc.pt>
2022-02-07 13:55:53 +0000

    Fix some minor problems

--- cv/views.py
+++ cv/views.py
@@ -6,11 +6,13 @@ def cv(request):
     languages=Language.objects.all()
     educations=Education.objects.all()
     works=Work.objects.all()
-    return render(request, "cv/cv.html" ,{'works':works,'languages':languages,'educations':educations,'skills':skills})
+    data={"work":"Informatic","email":"jpaguiar@gmail.com", "name":"José Pedro Aguiar","phone":"927396759","local":"Viseu,PT"}
+    return render(request, "cv/cv.html" ,{'works':works,'languages':languages,'educations':educations,'skills':skills,"data":data})
 # Create your views here.
 def wCv(request):
     skills=Skill.objects.all()
     languages=Language.objects.all()
     educations=Education.objects.all()
     works=Work.objects.all()
-    return render(request, "cv/white/cv.html" ,{'works':works,'languages':languages,'educations':educations,'skills':skills})
+    data={ "work":"Informatic","email":"jpaguiar@gmail.com", "name":"José Pedro Aguiar","phone":"927396759","local":"Viseu,PT"}
+    return render(request, "cv/white/cv.html" ,{'works':works,'languages':languages,'educations':educations,'skills':skills,"data":data})
