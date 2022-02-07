3400621688ac5a967da26b2f2a5af022c79b6ef9
José Pedro Aguiar <jpaguiar17@gmail.com>
2022-02-07 02:16:00 +0000

    Added a .gitignore

--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fpq1oxjtgc3d^t7xd^*v$+%p@lh=go=p^*cdq^*u@n_ip@9cwu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['josepedroaguiar.pythonanywhere.com']
 
 
 # Application definition
@@ -124,8 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL= '/img/'
-
+STATIC_ROOT= os.path.join(BASE_DIR,'static')
+MEDIA_URL= '/media/'
 MEDIA_ROOT= os.path.join(BASE_DIR,'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
