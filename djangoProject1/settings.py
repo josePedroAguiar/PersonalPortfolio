a5df77afc67db9476eb74add71d78c862b37e717
José Pedro Aguiar <88104767+josePedroAguiar@users.noreply.github.com>
2022-07-12 02:11:20 +0100

    Add requirements.txt

--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-fpq1oxjtgc3d^t7xd^*v$+%p@lh=go=p^*cdq^*u@n_ip@9cwu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
