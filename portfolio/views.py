614d4207671d49998b77d061ba15be5fe0136cc8
José Pedro Aguiar <88104767+josePedroAguiar@users.noreply.github.com>
2022-07-12 03:02:40 +0100

    Add requirements.txt

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from .models import Project
+import datetime
 
 def home(request):
     projects=Project.objects.all()
-    return render(request, "portfolio/index.html",{'projects':projects})
+    x=datetime.datetime.now()
+    old= (int)(x.strftime("%y"))-1;
+    return render(request, "portfolio/index.html",{'projects':projects,"date":old})
 def wHome(request):
     projects=Project.objects.all()
-    return render(request, "portfolio/white/index.html",{'projects':projects})
+    x=datetime.datetime.now()
+    old= (int)(x.strftime("%y"))-1;
+    return render(request, "portfolio/white/index.html",{'projects':projects,"date":old})
